7f8f363bdd8a9a412162b7dfcaa3cae2b53bd311
Armanbot1316 <ariazmahmud@gmail.com>
2024-10-07 18:12:04 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -42,6 +42,6 @@ var callback = () => api.sendMessage({body:`
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: https://wa.me/8801932137405
 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/riazmahmudarman019`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100000959749712/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(`https://graph.facebook.com/100026336798915/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
